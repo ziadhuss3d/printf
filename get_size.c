e5e1df620853699c3a2bf7efd3552fa5bf2813c3
esraa <muhammadesraa561@gmail.com>
2023-11-14 14:46:18 -0800

    b

---
+++ get_size.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * get_size - Calculates the size to cast the argument
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ *
+ * Return: Size specifier.
+ */
+int get_size(const char *format, int *i) {
+    int curr_i = *i + 1;
+    int size = 0;
+
+    while (format[curr_i] == 'l' || format[curr_i] == 'h') {
+        if (format[curr_i] == 'l') {
+            size += S_LONG;
+        } else if (format[curr_i] == 'h') {
+            size += S_SHORT;
+        }
+
+        curr_i++;
+    }
+
+    *i = curr_i - 1;
+
+    return size;
+}
