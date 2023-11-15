43d223fa5da01c68533d2c72efa06793ab07a5b9
ziadhuss3d <ziadhuss3d@example.com>
2023-11-14 18:37:03 -0800

    55555555555555

--- get_size.c
+++ get_size.c
@@ -7,21 +7,23 @@
  *
  * Return: Size specifier.
  */
-int get_size(const char *format, int *i) {
-    int curr_i = *i + 1;
-    int size = 0;
+int get_size(const char *format, int *i)
+{
+	int curr_i = *i + 1;
+	int size = 0;
 
-    while (format[curr_i] == 'l' || format[curr_i] == 'h') {
-        if (format[curr_i] == 'l') {
-            size += S_LONG;
-        } else if (format[curr_i] == 'h') {
-            size += S_SHORT;
-        }
-
-        curr_i++;
-    }
-
-    *i = curr_i - 1;
-
-    return size;
+	while (format[curr_i] == 'l' || format[curr_i] == 'h')
+	{
+		if (format[curr_i] == 'l')
+		{
+			size += S_LONG;
+		}
+		else if (format[curr_i] == 'h')
+		{
+			size += S_SHORT;
+		}
+		curr_i++;
+	}
+	*i = curr_i - 1;
+	return (size);
 }
