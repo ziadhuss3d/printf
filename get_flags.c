2de1f5a72d4ede85b1f3d566024021ff5dba84d4
ziadhuss3d <ziadhuss3d@example.com>
2023-11-15 16:27:01 -0800

    zezo

--- get_flags.c
+++ get_flags.c
@@ -15,7 +15,7 @@ int get_flags(const char *format, int *i)
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr_j = *i + 1; format[curr_j] != '\0'; curr_i++)
+	for (curr_j = *i + 1; format[curr_j] != '\0'; curr_j++)
 	{
 		for (j = 0; FLAGS_CH[j] != '\0'; j++)
 			if (format[curr_j] == FLAGS_CH[j])
@@ -28,7 +28,7 @@ int get_flags(const char *format, int *i)
 			break;
 	}
 
-	*i = curr_i - 1;
+	*i = curr_j - 1;
 
 	return (flags);
 }
